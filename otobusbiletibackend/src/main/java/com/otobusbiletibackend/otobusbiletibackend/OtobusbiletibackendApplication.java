c122418edc0f9ce7e2bbcdaa0f5543855e0a4e2e
Sedat Doğru <46866955+Sdtdogru@users.noreply.github.com>
2020-03-10 15:16:50 +0300

    1.0.0
    proje oluşturuludu

---
+++ otobusbiletibackend/src/main/java/com/otobusbiletibackend/otobusbiletibackend/OtobusbiletibackendApplication.java
@@ -0,0 +1,13 @@
+package com.otobusbiletibackend.otobusbiletibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtobusbiletibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtobusbiletibackendApplication.class, args);
+	}
+
+}
---
+++ otobusbiletibackend/src/test/java/com/otobusbiletibackend/otobusbiletibackend/OtobusbiletibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otobusbiletibackend.otobusbiletibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtobusbiletibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
